4e5907728197d5973a805c775fdef779370a12e6
DisiKharismaP <disi.kharisma@gmail.com>
2021-10-15 14:44:47 +0700

    commit

---
+++ lib/model/user_model.dart
@@ -0,0 +1,39 @@
+/// id : "Kka5AR3XLbgWYhQsGtTI"
+/// name : "Disi Kharisma"
+/// email : "disi.kharisma@gmail.com"
+/// password : "123456"
+/// goal : "Pro Women in Tech"
+
+class UserModel {
+  String? id;
+  String? name;
+  String? email;
+  String? password;
+  String? goal;
+  
+  UserModel({
+      this.id, 
+      this.name, 
+      this.email, 
+      this.password, 
+      this.goal,});
+
+  UserModel.fromJson(dynamic json) {
+    id = json['id'];
+    name = json['name'];
+    email = json['email'];
+    password = json['password'];
+    goal = json['goal'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['id'] = id;
+    map['name'] = name;
+    map['email'] = email;
+    map['password'] = password;
+    map['goal'] = goal;
+    return map;
+  }
+
+}
